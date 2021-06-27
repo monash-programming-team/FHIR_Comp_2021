eaf9cea7bc4134d18f72c027a30ec0dbc80906b1
glipR <37640160+glipR@users.noreply.github.com>
2021-06-27 11:22:32 +1000

    Fix read data on analysis.

--- solutions/Q4/analysis.py
+++ solutions/Q4/analysis.py
@@ -89,6 +89,7 @@ class Observation(Resource):
         self.patient = data["resource"]["subject"]["reference"].split(":")[-1]
         all_objects["patients"][self.patient].observations.append(self.id)
 import json
+from os import listdir
 from os.path import join as J
 
 def read_dataset(dataset_path):
@@ -98,33 +99,44 @@ def read_dataset(dataset_path):
     encounters = {}
     observations = {}
 
-    for objects, filename, klass in [
+    for objects, dirname, klass in [
         (organizations, "organizations", Organization),
         (practitioners, "practitioners", Practitioner),
         (patients, "patients", Patient),
         (encounters, "encounters", Encounter),
         (observations, "observations", Observation),
     ]:
-        # print("Reading file", filename)
+        # print("Reading file", dirname)
 
-        with open(J(dataset_path, filename + ".json"), "r") as f:
-            bundle_group = json.loads(f.read())["entry"]
+        for fname in listdir(J(dataset_path, dirname)):
+            with open(J(dataset_path, dirname, fname), "r") as f:
+                bundle_group = json.loads(f.read())["entry"]
 
-        all_objects = list(map(lambda o: klass(o, {
-            "organizations": organizations,
-            "practitioners": practitioners,
-            "patients": patients,
-            "encounters": encounters,
-            "observations": observations,
-        }), bundle_group))
-        for obj in all_objects:
-            objects[obj.id] = obj
+            all_objects = list(map(lambda o: klass(o, {
+                "organizations": organizations,
+                "practitioners": practitioners,
+                "patients": patients,
+                "encounters": encounters,
+                "observations": observations,
+            }), bundle_group))
+            for obj in all_objects:
+                objects[obj.id] = obj
 
     # Make sure that org and practitioner relations are sets.
     for org_obj in organizations.values():
         org_obj.practitioners = list(set(org_obj.practitioners))
     for prac_obj in practitioners.values():
         prac_obj.organizations = list(set(prac_obj.organizations))
+
+    """print(
+        "Read data:\n{} Organizations\n{} Practitioners\n{} Patients\n{} Encounters\n{} Observations".format(
+            len(organizations),
+            len(practitioners),
+            len(patients),
+            len(encounters),
+            len(observations),
+        )
+    )"""
 
     return {
         "organizations": organizations,
